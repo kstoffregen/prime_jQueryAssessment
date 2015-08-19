844b24183f2674d32adcffba9339c95d23193dc4
kstoffregen <stoffrk@Kathryns-MacBook-Pro.local>
2015-08-19 16:38:52 -0500

    Add toggleClass to switch colors.

--- scripts.js
+++ scripts.js
@@ -9,6 +9,7 @@ $(document).ready(function(){
 		$clickCount += 1;
 		$newDiv = $('<div id="newDiv">');
 		$newDiv.appendTo('body');
+		$newDiv.attr('class', 'colorOff');
 
 		$colorBtn.text('Change Color');
 		$removeBtn.text('Remove');
@@ -22,7 +23,8 @@ $(document).ready(function(){
 // The Change Color button should change the background color of the div container relative to the Change Color button that was clicked. For the purpose of this challenge, you can add extra functionality that when clicked again, it can change the color back to the original, BUT THIS IS NOT REQUIRED. Focus on core functionality first. Do extra stuff later if you have time.
 
 	$(document).on('click', '.color', function(){
-		$(this).parent().attr('class', 'colorOn');
+		// $(this).parent().attr('class', 'colorOn');
+		$(this).parent().toggleClass('colorOn', 'colorOff');
 	});
 
 // The Remove Button should remove the parent Div container of the button that was clicked, removing the entire div, which contains the text, and the two additional buttons (the Change Color and Remove buttons).
